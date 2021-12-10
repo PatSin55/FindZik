1790212b18a70f7a8460f44b3859fa678ce234e9
PatSin55 <kamdempatrick550@gmail.com>
2021-12-10 14:29:12 +0100

    premier push

---
+++ src/main/java/com/isj/Findzik/FindzikApplication.java
@@ -0,0 +1,13 @@
+package com.isj.Findzik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindzikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindzikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isj/Findzik/FindzikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isj.Findzik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindzikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
